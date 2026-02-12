c26d2d7f2c41ba2bb841c82913104b3c1cc97cdf
ftoutounchi <f.tutunchi@gmail.com>
2026-02-12 22:33:37 +0100

    version 2

--- sketch_feb12a/sketch_feb12a.ino
+++ sketch_feb12a/sketch_feb12a.ino
@@ -13,7 +13,7 @@
 const char* ssid     = "Paradise";
 const char* password = "Arezoo&Farzad7";
 
-long gmtOffset_sec = 0; 
+long gmtOffset_sec = 3600; 
 int  daylightOffset_sec = 0; 
 
 // ==========================================
@@ -30,7 +30,6 @@ WebServer server(80);
 // 3. LOGIC & STORAGE
 // ==========================================
 enum AlarmType { TYPE_ONCE, TYPE_DAILY, TYPE_WEEKLY, TYPE_MONTHLY };
-
 struct AlarmRule {
   int id;
   AlarmType type;
@@ -44,7 +43,9 @@ std::deque<String> pendingQueue;
 int nextRuleId = 1;
 int lastCheckedMinute = -1;
 unsigned long lastBlink = 0;
+unsigned long lastDisplayUpdate = 0;
 bool blinkState = false;
+bool isBusy = false; // NEW: Lock to prioritize WiFi
 
 void saveRules() {
   File f = LittleFS.open("/alarms.txt", "w");
@@ -91,89 +92,111 @@ String getPage() {
     html += "<div style='border-bottom:1px solid #eee;padding:8px 0;'><b>" + String(r.hour < 10 ? "0":"") + String(r.hour) + ":" + String(r.minute < 10 ? "0":"") + String(r.minute) + "</b> - " + r.message;
     html += " <a href='/del?id=" + String(r.id) + "'><button class='btn-del'>Del</button></a></div>";
   }
-  html += "</div><div class='card'><h3>Add New</h3><form action='/add' method='POST'>Msg: <input name='msg' required maxlength='12'>Time: <input type='time' name='time' required>";
+  html += "</div><div class='card'><h3>Add New</h3><form action='/add' method='POST' onsubmit='this.style.opacity=0.5'>Msg: <input name='msg' required maxlength='12'>Time: <input type='time' name='time' required>";
   html += "Type: <select name='type' onchange='updateFields(this.value)'><option value='1'>Daily</option><option value='0'>Once</option><option value='2'>Weekly</option><option value='3'>Monthly</option></select>";
-  html += "<div id='wd' style='display:none'>Days: S<input type='checkbox' name='w0'> M<input type='checkbox' name='w1'> T<input type='checkbox' name='w2'> W<input type='checkbox' name='w3'> T<input type='checkbox' name='w4'> F<input type='checkbox' name='w5'> S<input type='checkbox' name='w6'></div>";
+  html += "<div id='wd' style='display:none'>S<input type='checkbox' name='w0'>M<input type='checkbox' name='w1'>T<input type='checkbox' name='w2'>W<input type='checkbox' name='w3'>T<input type='checkbox' name='w4'>F<input type='checkbox' name='w5'>S<input type='checkbox' name='w6'></div>";
   html += "<div id='md' style='display:none'>Day: <input type='number' name='dom' min='1' max='31'></div><button type='submit' style='background:#5e2ca5;color:white;border:none;border-radius:4px;'>Save</button></form></div></body></html>";
   return html;
 }
 
 void handleAdd() {
+  isBusy = true; // Stop display
   AlarmRule r; r.id = nextRuleId++; r.message = server.arg("msg");
   r.hour = server.arg("time").substring(0,2).toInt(); r.minute = server.arg("time").substring(3,5).toInt();
   int t = server.arg("type").toInt(); r.type = (AlarmType)t;
   if(t == 2) for(int i=0; i<7; i++) r.weekdays[i] = server.hasArg("w" + String(i));
   if(t == 3) r.dom = server.arg("dom").toInt();
-  rules.push_back(r); saveRules(); server.sendHeader("Location", "/"); server.send(303);
+  rules.push_back(r); 
+  saveRules();
+  server.sendHeader("Location", "/");
+  server.send(303);
+  isBusy = false;
 }
 
 void handleDel() {
+  isBusy = true;
   int id = server.arg("id").toInt();
   for (auto it = rules.begin(); it != rules.end(); ++it) if (it->id == id) { rules.erase(it); break; }
-  saveRules(); server.sendHeader("Location", "/"); server.send(303);
-}
-
-void checkAlarms() {
-  struct tm ti; if(!getLocalTime(&ti)) return;
-  if(ti.tm_min == lastCheckedMinute) return;
-  lastCheckedMinute = ti.tm_min;
-  for (auto &r : rules) {
-    if(r.hour == ti.tm_hour && r.minute == ti.tm_min) {
-      bool trigger = false;
-      if(r.type == TYPE_DAILY) trigger = true;
-      else if(r.type == TYPE_WEEKLY && r.weekdays[ti.tm_wday]) trigger = true;
-      else if(r.type == TYPE_MONTHLY && r.dom == ti.tm_mday) trigger = true;
-      else if(r.type == TYPE_ONCE) trigger = true;
-      if(trigger) {
-        bool exists = false; for(auto &m : pendingQueue) if(m == r.message) exists = true;
-        if(!exists) { if(pendingQueue.size() >= 5) pendingQueue.pop_front(); pendingQueue.push_back(r.message); }
-      }
-    }
-  }
+  saveRules(); 
+  server.sendHeader("Location", "/");
+  server.send(303);
+  isBusy = false;
 }
 
 void setup() {
   Serial.begin(115200); pinMode(PIN_BUTTON, INPUT_PULLUP);
-  LittleFS.begin(true); loadRules(); u8g2.begin();
+  LittleFS.begin(true); loadRules(); 
+  Wire.begin(PIN_SDA, PIN_SCL);
+  Wire.setClock(400000); 
+  u8g2.begin();
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) delay(500);
   configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org");
   server.on("/", [](){ server.send(200, "text/html", getPage()); });
-  server.on("/add", handleAdd); server.on("/del", handleDel);
+  server.on("/add", handleAdd); 
+  server.on("/del", handleDel);
   server.begin();
 }
 
 void loop() {
-  server.handleClient(); checkAlarms();
-  if(digitalRead(PIN_BUTTON) == LOW) { if(!pendingQueue.empty()) { pendingQueue.pop_front(); delay(300); } }
+  server.handleClient(); 
+  
+  if (isBusy) return; // Prioritize WiFi requests over everything else
 
-  u8g2.clearBuffer();
-  struct tm ti;
-  if(getLocalTime(&ti)) {
-    char tStr[10], dStr[20];
-    sprintf(tStr, "%02d:%02d", ti.tm_hour, ti.tm_min);
-    strftime(dStr, 20, "%m/%d  %a", &ti); // Double space and full Weekday
-
-    if(pendingQueue.empty()) {
-      // TOP LINE: Date and Weekday (Full Width)
-      u8g2.setFont(u8g2_font_haxrcorp4089_tr); // Slightly larger, clear font
-      u8g2.drawStr(2, 9, dStr); 
-
-      // CENTER LINE: Big Clock
-      u8g2.setFont(u8g2_font_logisoso20_tn); // Increased from 16 to 20
-      u8g2.drawStr(1, 38, tStr); 
-    } else {
-      if(millis() - lastBlink > 500) { lastBlink = millis(); blinkState = !blinkState; }
-      if(blinkState) {
-         u8g2.setFont(u8g2_font_haxrcorp4089_tr);
-         u8g2.drawStr(2, 9, "--- ALARM ---");
-         u8g2.setFont(u8g2_font_logisoso20_tn);
-         u8g2.drawStr(1, 38, tStr);
-      } else {
-         u8g2.setFont(u8g2_font_7x14_tf); // Slightly taller message font
-         u8g2.drawStr(0, 26, pendingQueue.front().c_str());
+  unsigned long now = millis();
+  static unsigned long lastLogic = 0;
+  if(now - lastLogic > 1000) {
+    struct tm ti; 
+    if(getLocalTime(&ti)) {
+      if(ti.tm_min != lastCheckedMinute) {
+        lastCheckedMinute = ti.tm_min;
+        for (auto &r : rules) {
+          if(r.hour == ti.tm_hour && r.minute == ti.tm_min) {
+            bool trigger = false;
+            if(r.type == TYPE_DAILY) trigger = true;
+            else if(r.type == TYPE_WEEKLY && r.weekdays[ti.tm_wday]) trigger = true;
+            else if(r.type == TYPE_MONTHLY && r.dom == ti.tm_mday) trigger = true;
+            else if(r.type == TYPE_ONCE) trigger = true;
+            if(trigger) {
+              bool exists = false; for(auto &m : pendingQueue) if(m == r.message) exists = true;
+              if(!exists) { if(pendingQueue.size() >= 5) pendingQueue.pop_front(); pendingQueue.push_back(r.message); }
+            }
+          }
+        }
       }
     }
+    lastLogic = now;
   }
-  u8g2.sendBuffer();
+
+  if(digitalRead(PIN_BUTTON) == LOW) { 
+    if(!pendingQueue.empty()) { pendingQueue.pop_front(); delay(200); } 
+  }
+
+  if(now - lastDisplayUpdate >= 500) {
+    lastDisplayUpdate = now;
+    u8g2.clearBuffer();
+    struct tm ti;
+    if(getLocalTime(&ti)) {
+      char tStr[10], dStr[22];
+      sprintf(tStr, "%02d:%02d", ti.tm_hour, ti.tm_min);
+      strftime(dStr, 22, "%m/%d      %a", &ti);
+      if(pendingQueue.empty()) {
+        u8g2.setFont(u8g2_font_haxrcorp4089_tr); u8g2.drawStr(2, 9, dStr); 
+        u8g2.setFont(u8g2_font_logisoso20_tn); u8g2.drawStr(1, 38, tStr); 
+      } else {
+        blinkState = !blinkState;
+        if(blinkState) {
+         u8g2.setFont(u8g2_font_haxrcorp4089_tr);
+         u8g2.drawStr(18, 9, "!!! ALARM !!!"); 
+         u8g2.setFont(u8g2_font_logisoso20_tn);
+         u8g2.drawStr(1, 38, tStr);
+        } else {
+           u8g2.setFont(u8g2_font_9x15_tf);
+           int x = (72 - u8g2.getStrWidth(pendingQueue.front().c_str())) / 2;
+           u8g2.drawStr(max(0, x), 26, pendingQueue.front().c_str()); 
+        }
+      }
+    }
+    u8g2.sendBuffer();
+  }
 }
